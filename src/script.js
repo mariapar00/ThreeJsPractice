b1ca21035cb3b3c3d3ea2604d5df44b270e694fd
Maria Paraskevopoulou <paraskevopoulou@thinkdesquared.com>
2026-02-06 09:24:15 +0200

    lesson 03.5

--- src/script.js
+++ src/script.js
@@ -1,4 +1,6 @@
 import * as THREE from "three";
+// y-axis is up, x-axis is right, and z-axis is towards the viewer.
+// I choose my metric system
 
 // Canvas
 const canvas = document.querySelector("canvas.webgl");
@@ -13,9 +15,18 @@ const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 // Create mesh ----- Mesh(geometry, material)
 const mesh = new THREE.Mesh(geometry, material);
+mesh.position.x = 0.7; // Move the cube to the right
+mesh.position.y = -0.6; // Move the cube down
+mesh.position.z = 1; // Move the cube towards the viewer
+// Faster way:
+mesh.position.set(0.7, -0.6, 1); // Set the position using set(x, y, z)
 
 // Add mesh to scene
 scene.add(mesh);
+
+// console.log(mesh.position.length()); // 1.140175425099138
+// mesh.position.normalize(); // Normalize the position vector to have a length of 1
+// console.log(mesh.position.length()); // 1
 
 // Camera
 //Create the sizes
@@ -37,6 +48,10 @@ camera.position.z = 3;
 // Add camera to scene
 scene.add(camera);
 
+// Distance between camera and cube
+const distance = camera.position.distanceTo(mesh.position);
+console.log(distance); // 2.345207879911715
+
 // Renderer
 // Create renderer ----- WebGLRenderer(parameters)
 const renderer = new THREE.WebGLRenderer({ canvas });
@@ -44,3 +59,8 @@ renderer.setSize(sizes.width, sizes.height);
 
 // Render the scene from the perspective of the camera
 renderer.render(scene, camera);
+
+// I can define the positions anywhere in the code,
+// but I need to render the scene after defining the positions,
+// otherwise I won't see the changes.
+// Rendering is like taking a picture of the scene from the perspective of the camera.

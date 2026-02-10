12bf37709a25ddc4164743d965e20168d75e3c11
Maria Paraskevopoulou <paraskevopoulou@thinkdesquared.com>
2026-02-10 21:41:29 +0200

    lesson 9

--- 09-debug-ui/src/script.js
+++ 09-debug-ui/src/script.js
@@ -4,6 +4,7 @@ import gsap from "gsap";
 import GUI from "lil-gui";
 
 const gui = new GUI();
+const debugObject = {};
 
 /**
  * Base
@@ -17,11 +18,68 @@ const scene = new THREE.Scene();
 /**
  * Object
  */
+debugObject.color = "#ff0000";
 const geometry = new THREE.BoxGeometry(1, 1, 1, 2, 2, 2);
-const material = new THREE.MeshBasicMaterial({ color: "#ff0000" });
+const material = new THREE.MeshBasicMaterial({ color: debugObject.color });
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
+gui.add(mesh.position, "y").min(-3).max(3).step(0.01).name("elevation");
+
+gui.add(mesh, "visible");
+
+gui.add(material, "wireframe");
+
+gui
+  .addColor(material, "color") // Will open a color picker but the color in the tweak is not the same
+  // as the one in the scene, because the color in the scene is converted to a hexadecimal value,
+  // but the color in the tweak is not.
+  // To fix this, we can use the onChange method to update the color in the scene
+  // when the color in the tweak changes:
+  .onChange((value) => {
+    // value here is the material.color
+    console.log(value.getHexString());
+    // but this is still not a solutuon because we need to take the hex code and put it manually
+    // in the color picker, so we can use the onChange method to update the color in the scene
+  });
+
+// Solution:
+gui.addColor(debugObject, "color").onChange((value) => {
+  material.color.set(value);
+});
+
+// Spin animation
+debugObject.spin = () => {
+  gsap.to(mesh.rotation, {
+    duration: 1,
+    y: mesh.rotation.y + Math.PI * 2
+  });
+};
+gui.add(debugObject, "spin").name("Spin");
+
+// Tweak geometry
+// Tweaking the segments
+debugObject.subdivision = 2;
+gui
+  .add(debugObject, "subdivision")
+  .min(1)
+  .max(20)
+  .step(1)
+  .onFinishChange(() => {
+    // not onChange because we don't want to update the geometry on every
+    // change (lengthy process for the CPU), but only when the user finishes changing the value
+
+    mesh.geometry.dispose(); // dispose the old geometry to free up memory
+
+    mesh.geometry = new THREE.BoxGeometry(
+      1, // width
+      1, // height
+      1, // depth
+      debugObject.subdivision, // width segments
+      debugObject.subdivision, // height segments
+      debugObject.subdivision // depth segments
+    );
+  });
 /**
  * Sizes
  */
